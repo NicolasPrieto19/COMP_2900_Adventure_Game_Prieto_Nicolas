fb8c9a57052726a77cead9755a855f33e3c56762
Nicolas_Prieto_Diaz <NPrieto5685@interbayamon.edu>
2026-05-04 16:41:59 -0400

    Anadiendo clase Adventurer

--- src/AdventureGame/Program.cs
+++ src/AdventureGame/Program.cs
@@ -4,8 +4,14 @@ public class Program
 {
     public static void Main()
     {
-        Room r = new Room();
+        Adventurer a = new Adventurer();
+        a.SetLamp(true);
+        a.SetKey(true);
 
+        Room r = new Room();
+        r.SetDescription("Room 1");
+        r.SetLamp(true);
+        Console.WriteLine(a);
         Console.WriteLine(r);
     }
 }
